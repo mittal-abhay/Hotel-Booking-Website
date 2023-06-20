1ac8f9a43b027c5087e9897a9697f9e4c5f52b7f
ABHAY MITTAL <abhayhmittal@gmail.com>
2023-06-20 10:44:11 +0530

    error fixing

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -24,7 +24,6 @@ const Login = () => {
     try {
       const res = await axios.post("/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-    //   console.log(res)
       navigate("/")
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
