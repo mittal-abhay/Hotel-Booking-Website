e5e855582918385061522a4e4d68101599d2bc33
ABHAY MITTAL <abhayhmittal@gmail.com>
2023-06-20 11:01:47 +0530

    m

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -18,7 +18,7 @@ const List = () => {
   const [max, setMax] = useState(null);
 
   const { data, loading, error, reFetch } = useFetch(
-    `/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
+    `https://hotel-booking-website-ssom.onrender.com/api/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
   );
 
   const handleClick = () => {
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post("https://hotel-booking-website-ssom.onrender.com/api/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       navigate("/")
     } catch (err) {
